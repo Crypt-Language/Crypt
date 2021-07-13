d93d98a7c67a6efb109df05fcfbc7e5e6a09f31d
AurumByte <80045511+AurumByte@users.noreply.github.com>
2021-07-13 17:01:33 +0530

    switch to maven, indent alg fix

---
+++ src/main/java/dev/aurumbyte/crypt/CryptCompiler/utils/VariableSymbolTable.java
@@ -0,0 +1,48 @@
+package dev.aurumbyte.crypt.CryptCompiler.utils;
+
+import dev.aurumbyte.crypt.CryptCompiler.Lexer.token.Token;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VariableSymbolTable {
+    private final VariableSymbolTable enclosing;
+    private final Map<String, Object> values = new HashMap<>();
+
+    public VariableSymbolTable() {
+        enclosing = null;
+    }
+
+    VariableSymbolTable(VariableSymbolTable enclosing) {
+        this.enclosing = enclosing;
+    }
+
+    Object get(Token name) {
+        if (values.containsKey(name.lexeme)) {
+            return values.get(name.lexeme);
+        }
+
+
+        if (enclosing != null) return enclosing.get(name);
+
+        throw new Error("Undefined variable '" + name.literal + "'.");
+    }
+
+    void define(String name, Object value) {
+        values.put(name, value);
+    }
+
+    void assign(Token name, Object value) {
+        if (values.containsKey(name.lexeme)) {
+            values.put((String) name.lexeme, value);
+            return;
+        }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+
+        throw new Error("Undefined variable '" + name.literal + "'.");
+    }
+}

a34df42d6430e481ca98819ceb984557c680f517
AurumByte <80045511+AurumByte@users.noreply.github.com>
2021-09-09 16:20:26 +0530

    Delete src/main/java/crypt/language directory

--- src/main/java/crypt/language/parser/environments/type/Types.java
+++
@@ -1,5 +0,0 @@
-package crypt.language.parser.environments.type;
-
-public enum Types {
-    INT, FLOAT, DOUBLE, STRING
-}
--- src/main/java/crypt/language/parser/environments/type/classType/ClassType.java
+++
@@ -1,7 +0,0 @@
-package crypt.language.parser.environments.type.classType;
-
-public enum ClassType {
-    NONE,
-    CLASS,
-    SUBCLASS
-}
